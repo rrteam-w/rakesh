b6dc9ec286b7ba66b9662ece4aeba9db52e63c01
Rakesh-wipro <rakesh.yenjala@wipro.com>
2020-04-03 13:46:48 +0530

    delete

--- SpringBootJPA/src/main/java/com/pnc/springboot/SpringBootJPA/repository/DAORepository.java
+++
@@ -1,9 +0,0 @@
-package com.pnc.springboot.SpringBootJPA.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.pnc.springboot.SpringBootJPA.model.User;
-
-public interface DAORepository extends CrudRepository<User, String>{
-
-}
--- SpringBootJWT/src/main/java/com/pnc/springboot/SpringBootJWT/config/SecurityConstants.java
+++
@@ -1,5 +0,0 @@
-package com.pnc.springboot.SpringBootJWT.config;
-
-public class SecurityConstants {
-	 public static final String TOKEN_PREFIX = "Bearer ";
-}
